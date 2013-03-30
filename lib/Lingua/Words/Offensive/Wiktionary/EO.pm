550a1e0a56a54ccd29190758c6d3d75b89d07a9e
Nick Patch <patch@cpan.org>
2013-03-30 10:30:31 -0400

    add eo

---
+++ lib/Lingua/Words/Offensive/Wiktionary/EO.pm
@@ -0,0 +1,61 @@
+package Lingua::Words::Offensive::Wiktionary::EO;
+
+use v5.6;
+use strict;
+use warnings;
+use utf8;
+
+our $VERSION = '0.00_1';
+
+# retrieved 2013-03-30
+our @words = qw(
+    ĉuras
+    ĉuri
+    ĉuris
+    ĉuro
+    ĉuron
+    ĉuros
+    ĉuru
+    ĉurus
+    fek
+    fekaĵo
+    fekaĵon
+    fekas
+    feki
+    fekis
+    fekos
+    feku
+    fekus
+    fika
+    fikaj
+    fikajn
+    fikan
+    fikas
+    fiki
+    fikis
+    fiko
+    fikoj
+    fikojn
+    fikon
+    fikos
+    fiku
+    fikus
+    kaco
+    kacoj
+    kacojn
+    kacon
+    kojono
+    kojonoj
+    kojonojn
+    kojonon
+    piĉo
+    piĉoj
+    piĉojn
+    piĉon
+    pugtruo
+    pugtruoj
+    pugtruojn
+    pugtruon
+);
+
+1;
